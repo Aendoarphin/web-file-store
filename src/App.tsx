3ce9acdcb30e1272088861010f4056ef1e458220
Aendoarphin <aronp2000@gmail.com>
2025-03-05 20:31:41 -0600

    add test code to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useEffect, useState } from "react";
+import supabase from "./utils/supabase";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState<any[] | null>(null);
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  async function getUsers() {
+    const { data } = await supabase.from("users").select();
+    setUsers(data);
+  }
 
   return (
-    <>
-      <div className="text-red-500">hello</div>
-    </>
+    <ul>
+      {users && users.map((user) => (
+        <p key={user.id}>
+          {user.id}
+          <br />
+          {user.first_name}
+          <br />
+          {user.last_name}
+          <br />
+          {user.username}
+          <br />
+          {user.email}
+          <br />
+          <br />
+        </p>
+      ))}
+    </ul>
   );
 }
 
