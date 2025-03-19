52e545cb2328663e3bad9207bc734454ced5fb9b
Aendoarphin <aronp2000@gmail.com>
2025-03-18 19:53:15 -0500

    work on home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,13 +10,17 @@ const Home = () => {
 	const handleSubmit = async () => {
 		if (input) {
 			const response = await searchFile(input);
-			setData(response);
+      setData(response);
+      if (response.length === 0) {
+        alert("No results found");
+        setData(["No results found"]);
+      }
 		}
 	}
 
 	return (
 		<>
-			<div className="flex-row gap-4 flex justify-between border items-center p-6 w-full">
+			<div className="flex-row gap-4 flex justify-between items-center p-6 w-full">
 				<div className="flex flex-row justify-between gap-2">
 					<h1 className="font-extrabold">SOPY</h1>
 				</div>
@@ -34,7 +38,7 @@ const Home = () => {
 						}}
 						className={`bg-neutral-300 outline-0 focus:border-[1px] ${
 							input.length > 0 ? "rounded-l-lg" : "rounded-lg"
-						} p-2 text-sm min-w-96`}
+						} p-2 text-sm`}
 						placeholder="How to fund a loan..."
 						onChange={(e) => {
 							setInput(e.target.value);
@@ -42,7 +46,7 @@ const Home = () => {
 					/>
 					{input && (
 						<button
-							className={`bg-neutral-600 active:scale-95 text-white outline-0 font-bold py-2 px-4 ${
+							className={`bg-neutral-900 active:scale-95 text-white outline-0 font-bold py-2 px-4 ${
 								input.length > 0 ? "rounded-r-lg" : "rounded-lg"
 							}`}
 							onClick={() => handleSubmit()}
@@ -52,10 +56,11 @@ const Home = () => {
 					)}
 				</div>
 			</div>
-			<div className="flex flex-col items-start p-6 w-full">
+      <hr className="mx-4 text-neutral-400"/>
+			<div id="resultsContainer" className="flex flex-col items-start p-6 w-full">
 				{data?.map((item) => (
 					<ResultItem fileName={item} />
-				))}
+        ))}
 			</div>
 		</>
 	);
